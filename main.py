1b6dac1cf285cd0c6eae3ada3c5ebfd17159f61c
Jared <jaredbbowden@gmail.com>
2018-03-03 15:18:42 -0600

    First test

--- main.py
+++ main.py
@@ -3,45 +3,29 @@
 import pandas as pd
 import datetime
 
+
+# Read in the log file
+big_log = pd.read_csv("./big_log.csv")
+
+# Time variables
 now = datetime.datetime.now()
 now.strftime("%Y-%m-%d %H:%M")
 
+# Populate new values to new entry
+new_entry = pd.DataFrame(
+    data={
+        "Date": now.strftime("%Y-%m-%d %H:%M"),
+        "Weight": input("Current weight?: "),
+        "Type": input("Is this a run, bike, or log?: "),
+        "Time": input("How long did you exercise?: "),
+        "Pace": input("What was your pace?: "),
+        "Comment": input("Comments?: "),
+    },
+    index=[0])
 
-# Init the data frame to log to
-starting_out = pd.DataFrame(columns={
-    "Date": [],
-    "Type": [],
-    "Weight": [],
-    "Time": [],
-    "Pace": [],
-    "Comment": []
-})
-starting_out.to_csv("./big_log.csv")
-
-another = pd.DataFrame(data={
-    "Date": now.strftime("%Y-%m-%d %H:%M"),
-    "Weight": input("What is your name? "),
-    "Type": input("Is this a run, bike, or log? "),
-    "Time": input("How long did you exercise? "),
-    "Pace": input("What was your pace? "),
-    "Comment": input("Comments? ")
-})
+# Write out new log
 
 # Append the result
-sure = starting_out.append(another, ignore_index=True)
-
-input("What is your name? ")
-
-another = pd.DataFrame(data={
-    "Date": [1],
-    "Weight": [1],
-    "Type": [1],
-    "Time": [1],
-    "Pace": [1],
-    "Comment": [1]
-})
+sure = big_log.append(new_entry, ignore_index=True)
 
 print(sure)
-
-
-now.strftime("%Y-%m-%d %H:%M")
